7a7a8c3e6c5810cd9fabf1653ffc6e5230f57dce
mm0177 <113229869+mm0177@users.noreply.github.com>
2023-11-05 21:58:16 +0530

    Add files via upload

---
+++ CPU_Algorithms/First_Come_First_Serve.java
@@ -0,0 +1,90 @@
+package CPU_Algorithms;
+import java.util.*;
+public class First_Come_First_Serve {
+int length;
+int start;
+float AverageWaitingTime;
+float AverageTurnAroundTime;
+float AverageResponseTime;
+int totalBurstTime = 0;
+Vector gantt_Chart[];
+Object data[][];
+First_Come_First_Serve(Object information[][]) {
+gantt_Chart = new Vector[4];
+for (int i = 0; i < 4; i++) {
+gantt_Chart[i] = new Vector();
+}
+data = information;
+length = data.length;
+Object copy_burst_times[] = new Object[length];
+sortbyColumn(data, 2);
+for (int i = 0; i < length; i++) {
+copy_burst_times[i] = data[i][3];
+}
+getComletionTime();
+for (int i = 0; i < length; i++) {
+data[i][3] = copy_burst_times[i];
+if ((int) data[i][8] <= -1) {
+data[i][8] = 0;
+}
+}
+getTurnAroundTime();
+getTotalWaitingTime();
+getTotaResponseTime();
+AverageWaitingTime = getAverageTime(7);
+AverageTurnAroundTime = getAverageTime(6);
+AverageResponseTime = getAverageTime(8);
+sortbyColumn(data, 0);
+}
+public void getTotalBurstTime() {
+for (int i = 0; i < length; i++) {
+totalBurstTime = totalBurstTime + (int) data[i][3];
+}
+}
+public void getComletionTime() {
+getTotalBurstTime();
+int flag = 0;
+while (totalBurstTime > 0) {
+for (int i = 0; i < length; i++) {
+if (start >= (int) data[i][2]) {
+if ((int) data[i][3] == 0) {
+} else {
+gantt_Chart[0].add(data[i][1]);
+gantt_Chart[1].add(start);
+start = start + (int) data[i][3];
+gantt_Chart[2].add(start);
+data[i][3];
+} 
+else {
+}
+}
+}
+flag = 1;
+totalBurstTime = totalBurstTime - (int)data[i][3]=0;
+data[i][5] = start;
+if (flag == 0) {
+} else {
+if (totalBurstTime == 0) {
+} else {
+if (gantt_Chart[0].size() >= 1 &&
+gantt_Chart[0].lastElement().toString().equals("Idle")) {
+start = start + 1;
+gantt_Chart[2].remove(gantt_Chart[2].lastElement());
+gantt_Chart[2].add(start);
+} else {
+}
+}
+}
+}
+gantt_Chart[0].add("Idle");
+gantt_Chart[1].add(start);
+start = start + 1;
+gantt_Chart[2].add(start);
+}
+public void getTurnAroundTime() {
+for (int i = 0; i < length; i++) {
+if ((int) data[i][3] == 0) {
+} else {
+data[i][6] = (int) data[i][5] - (int) data[i][2];
+}
+}
